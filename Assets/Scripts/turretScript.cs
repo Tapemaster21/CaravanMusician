0975eb55afb221a6285f5abd92763405f2b5ef8f
schiggity <spug911@gmail.com>
2017-06-07 11:35:09 -0500

    schig- made tower that follows

---
+++ Assets/Scripts/turretScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretScript : MonoBehaviour {
+
+	public float damage;
+	
+	
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		
+	}
+	
+	void OnTriggerEnter(Collider target)
+	{
+	//	transform.LookAt (target.transform);
+	//	target.BroadcastMessage("Damage", damage, SendMessageOptions.DontRequireReceiver);	
+	}
+
+	void OnTriggerStay(Collider target){
+		Vector3 targetPostition = new Vector3(target.transform.position.x, this.transform.position.y, target.transform.position.z);
+		this.transform.LookAt(targetPostition) ;
+
+		//transform.LookAt (target.transform);
+		//transform.RotateAround (transform.parent.up, Vector3.up, (transform.forward));
+	}
+}
